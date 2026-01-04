379afdd05f94a9a06d70ee61816c75f10b7f2517
shmavonyankaren <2003.karen.shmavonyan.com>
2026-01-04 10:23:18 +0400

    error fix

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import React from "react";
 import { ContactHeader } from "@/components/contact/ContactHeader";
 import { QuickActions } from "@/components/contact/QuickActions";
